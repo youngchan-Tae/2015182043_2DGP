e3f7fd9b52c82152361df6f2caae425a1c240f98
youngchan-Tae <1140116@naver.com>
2018-09-10 17:05:52 +0900

    소년 움직임

---
+++ Drills/Drill-03/character_moves.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+
+while (x < 800):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x,  90)
+    x = x + 2
+    delay(0.01)
+
+    if(x == 800):
+        x = 0
+    
+close_canvas()
